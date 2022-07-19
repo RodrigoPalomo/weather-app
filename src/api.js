0eeb861b348b54efbef3d02a9fb98d258d2c5049
Rodrigo Palomo <repmlm@hotmail.com>
2022-07-19 16:59:32 -0300

    all

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '978872bab8msh5d31ee399b06748p193890jsn85d7049856c2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6898fc6fc78ce37134533b9a4b03aaf9";
